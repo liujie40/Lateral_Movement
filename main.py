8607ac3672c7e2539c7dabd8e34f36d10cb8a17e
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2020-05-31 13:45:32 -0400

    tkinter imported

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 from settings import *
+from ui import *
+
+
+def main():
+    root = Tk()
+
+    root.mainloop()
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ ui.py
@@ -0,0 +1,7 @@
+from tkinter import *
+from tkinter import ttk
+
+class UI:
+
+    def __init__(self, master):
+        pass
